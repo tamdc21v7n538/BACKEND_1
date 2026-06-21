aaefb78cc5e7dd11c3193fe0195b69795c7590c4
tamdc21v7n538 <tamdc21v7n538@vlvh.ctu.edu.vn>
2026-06-21 19:36:34 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
+
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
+
 
 app.use(cors());
 app.use(express.json());
@@ -11,5 +14,16 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+    });
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+        });
+});
+
 
 module.exports = app;
